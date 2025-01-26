1eb781591f461781892a7ff9f0dbc85434c48f78
Leon Shimizu <lmshimizu@gmail.com>
2025-01-26 19:37:30 +1000

    Fix the syntax in the cors to fix the cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('https://hafaloha-reservations.netlify.app', 'http://localhost:5173')
+    origins 'https://hafaloha-reservations.netlify.app', 'http://localhost:5173'
 
     resource '*',
       headers: :any,
