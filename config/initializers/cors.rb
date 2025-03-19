aa034e3580b3035d04b1f4cfbceebf0cf791f20a
Leon Shimizu <lmshimizu@gmail.com>
2025-03-19 16:24:38 +1000

    add the new site name to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   # For backward compatibility, keep the original origins
   allow do
-    origins "https://hafaloha.netlify.app", "https://hafaloha-lvmt0.kinsta.page"
+    origins "https://hafaloha.netlify.app", "https://hafaloha-lvmt0.kinsta.page", "https://hafaloha-orders.com"
 
     resource "*",
       headers: :any,
